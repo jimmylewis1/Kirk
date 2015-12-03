b33431bcb2a016987fa86033912f12829c6bcb7a
jimmylewis1 <jimmy@getweave.com>
2015-12-03 15:12:23 -0700

    sign-in via Google functional. profile pic via Google.

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -2,10 +2,21 @@ var app = angular.module('weaveSearch');
 
 app.controller('mainCtrl', function($scope, mainService, Auth){
     
-    $scope.login = function() {
-        Auth.$authWithOAuthPopup("google").then(function(authData) {
-            console.log(authData);
-        }).catch(function(error) {
+    $scope.appointments = function() {
+        mainService.getAppts().then(function(response) {
+            $scope.appointments = response.data;
+        });
+    }
+    $scope.appointments();
+    
+    Auth.$onAuth(function(authData) {
+        $scope.authData = authData;
+        console.log(authData);
+    })
+    
+     $scope.login = function() {
+        Auth.$authWithOAuthPopup("google")
+        .catch(function(error) {
             console.error(error);
         });
     }
@@ -13,12 +24,5 @@ app.controller('mainCtrl', function($scope, mainService, Auth){
     $scope.logout = function() {
         Auth.$unauth();
     }
-    
-    $scope.appointments = function() {
-        mainService.getAppts().then(function(response) {
-            $scope.appointments = response.data;
-        });
-    }
-    $scope.appointments();
 });
 
